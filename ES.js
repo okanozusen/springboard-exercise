622e92a67487708af8c8243483af4122f80633ef
okan ozusen <okanozusen@hotmail.com>
2024-06-02 16:12:05 -0400

    Add files via upload

---
+++ ES.js
@@ -0,0 +1,12 @@
+let PI = 3.14;
+PI = 42;
+
+/* 1.The difference between Let and var is that with var you can redeclare while let you cant. Also var can you use hoisting varibable while let creats code block
+
+2.The difference between const and var is that with var you can redeclare while const you cant. Also var can you use hoisting varibable while const creats code block
+
+3. The difference between const and let is that with const is absolute cannot change or mutate but with let you can redeclare or reassign using let
+
+4. Hoisting is having the scope is provide the scoping later on while var let you use it before 
+*/
+
